569c6aca63398223ef45cf6cd55e2afd710fb062
Kaelon Townsend <kaelontownsend@gmail.com>
2024-02-07 15:22:59 -0600

    Add infinite tries and q to exit

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,16 @@
+def guess_number(number):
+    choice = input("Enter q to quit guessing and to continue enter anyhing else: ")
+    if choice == 'q':
+        print(f"Sorry! The number was {number}.")
+    else:
+        print("I'm thinking of a number...")
+        guess = int(input("What number am I thinking of? "))
+
+        if guess == number:
+            print("Congratulations! You guessed the right number.")
+        else:
+            print("Wrong try again")
+            guess_number(number)
+
 number = 10
-
-print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
-
-if guess == number:
-   print("Congratulations! You guessed the right number.")
-else:
-   print(f"Sorry! The number was {number}.")
+guess_number(number)
